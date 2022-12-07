49a85575bf2711a71252f3c95dae0beb3e0da393
EELMMAD <elmiramadadi@gmail.com>
2022-12-07 18:59:56 +0100

    Multiple type parameters

---
+++ src/main/java/se/mosh/elmira/generics/KeyValuePair.java
@@ -0,0 +1,11 @@
+package se.mosh.elmira.generics;
+
+public class KeyValuePair <K, V>{
+    private K key;
+    private V value;
+
+    public KeyValuePair(K key, V value){
+        this.key = key;
+        this.value = value;
+    }
+}
--- src/main/java/se/mosh/elmira/generics/Main.java
+++ src/main/java/se/mosh/elmira/generics/Main.java
@@ -32,11 +32,17 @@ public class Main {
 
         */
 
+
+        /*
         var max = Utils.max(1, 3);
         System.out.println(max);
 
 
         var max2 = Utils.max(new User(10), new User(20));
         System.out.println(max2);
+
+         */
+
+        Utils.print(1, 10);
     }
 }
