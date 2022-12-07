446c927a88cbeef0e274b46b920fcaa53e063484
EELMMAD <elmiramadadi@gmail.com>
2022-12-07 19:24:53 +0100

    Generic classes and inheritance

---
+++ src/main/java/se/mosh/elmira/generics/Instructor.java
@@ -0,0 +1,7 @@
+package se.mosh.elmira.generics;
+
+public class Instructor extends User{
+    public Instructor(int points) {
+        super(points);
+    }
+}
--- src/main/java/se/mosh/elmira/generics/Main.java
+++ src/main/java/se/mosh/elmira/generics/Main.java
@@ -43,6 +43,18 @@ public class Main {
 
          */
 
+        /*
         Utils.print(1, 10);
+         */
+
+        /*
+        User user = new Instructor(10);
+        Utils.printUser(new Instructor(10));
+
+         */
+
+        var instructors = new GenericList<Instructor>();
+        var users = new GenericList<User>();
+        Utils.printUser(users);
     }
 }
