491c172ca5a964421f9eaffeb794d516cbfdeee8
EELMMAD <elmiramadadi@gmail.com>
2022-12-07 16:10:56 +0100

    Generics and Primitive types

--- src/main/java/se/mosh/elmira/generics/Main.java
+++ src/main/java/se/mosh/elmira/generics/Main.java
@@ -2,8 +2,20 @@ package se.mosh.elmira.generics;
 
 public class Main {
     public static void main(String[] args) {
+        /*
        var list = new GenericList<User>();
        list.add(new User());             //we do NOT need an explicit cast
       User user =  list.get(0);
+         */
+
+
+        //         Reference type
+        // int -> Integer
+        //float -> Float
+        //boolean -> Boolean
+        GenericList<Integer> numbers = new GenericList<>();
+        numbers.add(1); //Boxing
+        int number = numbers.get(0); //Unboxing
+
     }
 }
